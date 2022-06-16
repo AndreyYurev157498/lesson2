eee8984ace6271ebe9d8dbfe690b1664c2591f40
andrey <jur_frame@mail.ru>
2022-06-16 18:56:33 +0400

    Решение первой Задачи

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число ");
+string paramStr = Console.ReadLine();
+int paramInt = int.Parse(paramStr);
+
+if (paramStr.Length != 3)
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
+else
+Console.WriteLine("Вторая цифра числа равна " + (paramInt % 100) / 10);
